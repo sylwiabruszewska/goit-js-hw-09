d91d895750073000108423c73019559677873d3e
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-06-26 16:27:01 +0200

    feat: add function to create promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,17 @@
+const form = document.querySelector('.form');
+const delayInput = document.querySelector('input[name="delay"]');
+const stepInput = document.querySelector('input[name="step"]');
+const amountInput = document.querySelector('input[name="amount"]');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
