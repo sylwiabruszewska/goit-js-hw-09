4a0969f72184b1df3df47b464f20b1f5386788b7
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-06-26 12:43:16 +0200

    feat: add variables and functions for random color and change bg color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,12 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const bodyElement = document.body;
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBackgroundColor() {
+  bodyElement.style.backgroundColor = getRandomHexColor();
+}
