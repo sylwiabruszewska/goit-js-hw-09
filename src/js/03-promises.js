2bb1777996746b8a58b95a7f266c928bcf3e73ec
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-06-26 17:20:34 +0200

    import notiflix and replace console logs with Notiflix notifications

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 const form = document.querySelector('.form');
 const delayInput = document.querySelector('input[name="delay"]');
 const stepInput = document.querySelector('input[name="step"]');
@@ -29,10 +31,14 @@ const onSubmit = event => {
 
     createPromise(position, promiseDelay)
       .then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
       })
       .catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
       });
   }
 };
